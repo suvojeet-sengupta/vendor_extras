99498305b58c497fabcae53a0b3903df9e75d98d
TopexGuy <topexguy@gmail.com>
2026-04-03 16:33:09 +0530

    extras: adding initial script

---
+++ prebuilts.mk
@@ -0,0 +1,25 @@
+PRODUCT_SOONG_NAMESPACES += vendor/extras
+PRODUCT_SOONG_NAMESPACES += external/vanadium
+
+ifeq ($(SGC),true)
+PRODUCT_PACKAGES += \
+    SGCam
+endif
+
+ifeq ($(SUVM),true)
+PRODUCT_PACKAGES += \
+    SuvMusic
+endif
+
+ifeq ($(KSUN),true)
+PRODUCT_PACKAGES += \
+    KernelSU-Next
+endif
+
+ifeq ($(VND),true)
+PRODUCT_PACKAGES += \
+    TrichromeLibrary \
+    TrichromeWebView \
+    TrichromeChrome \
+    VanadiumConfig
+endif
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+git clone https://gitlab.com/grapheneos/platform_external_vanadium.git external/vanadium
